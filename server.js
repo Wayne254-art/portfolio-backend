37c78a1471a61b64664fa9bab796aeb21482215f
Wayne254-art <waynegiyabe6@gmail.com>
2025-05-13 02:47:57 +0300

    Initial commit for backend

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const app = express();
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://wayne-marwa-portfolio.web.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
