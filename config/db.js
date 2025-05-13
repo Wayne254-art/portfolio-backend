5ee3cff935f88c5f3b18516f80ab3d9715f58ee1
Wayne254-art <waynegiyabe6@gmail.com>
2025-05-13 12:36:09 +0300

    added variables

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('portfolio', 'Wayne', '', {
-  host: '127.0.0.1',
-  port: '3306',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
+  port: process.env.DB_PORT,
   logging: false,
 });
 
